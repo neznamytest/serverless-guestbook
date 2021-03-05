11b0eb30a6d684d69ef2514beaf51671f0fc0972
neznamytest <79858895+neznamytest@users.noreply.github.com>
2021-03-05 17:54:19 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://1c80ece8.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
