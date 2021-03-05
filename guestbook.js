fe7faee8b9219d468fb73369a99ff84026a59b78
neznamytest <79858895+neznamytest@users.noreply.github.com>
2021-03-05 17:55:44 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://github.com/neznamytest/serverless-guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
